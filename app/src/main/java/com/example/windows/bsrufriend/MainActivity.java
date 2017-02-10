e7b9d13db9adc1d154056cfb5ba8caba75501346
yingkatii <yingkantima37@gmail.com>
2017-02-10 11:30:17 +0700

    image OK

--- app/src/main/java/com/example/windows/bsrufriend/MainActivity.java
+++ app/src/main/java/com/example/windows/bsrufriend/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.windows.bsrufriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit การประกาศตัวเเปร
+    private Button signInButton, signUpButton;
+    private EditText userEditText, passEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Bind Widget คือการ Initial Var กับ View บน XML
+        signInButton = (Button) findViewById(R.id.button2);
+        signUpButton = (Button) findViewById(R.id.button);
+        userEditText = (EditText) findViewById(R.id.editText);
+        passEditText = (EditText) findViewById(R.id.editText2);
+
+        // Button Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+    } // Main Method
+} // Main Class นี่คือ คลาสหลัก
---
+++ app/src/main/java/com/example/windows/bsrufriend/SignUpActivity.java
@@ -0,0 +1,113 @@
+package com.example.windows.bsrufriend;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class SignUpActivity extends AppCompatActivity {
+    //Explicit
+    private EditText nameEditText, userEditText, passEditText;
+    private ImageView imageView;
+    private RadioGroup radioGroup;
+    private Button button;
+    private String nameString, userString, passString;
+    private Uri uri;
+    private boolean aBoolean = true;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        bindWidget();
+        //Button Controller
+        buttonController();
+
+        //Image Controller
+        ImageController();
+
+    }   //main Method
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            aBoolean = false;
+            uri = data.getData();
+            //Setup Image Choose to ImageView
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+
+
+        } //if
+    } //onActivityResult
+
+    private void ImageController() {
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "โปรดเลือกแอพดูภาพ"), 1);
+            }
+        });
+    }
+
+    private void buttonController() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get value
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passString = passEditText.getText().toString().trim();
+
+                //Check Space
+                if (nameString.equals("") || userString.equals("") || passString.equals("")) {
+                    // True ==> Have Space
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this);
+                    myAlert.myDialog("มีช่องว่าง", "กรุณากรอกให้ครบทุกช่องค่ะ");
+                } else if (aBoolean) {
+                    //non
+                    MyAlert myAlert1 = new MyAlert(SignUpActivity.this);
+                    myAlert1.myDialog("ยังไม่เลือกรูปภาพ", "กรุณาเลือกรูปภาพสิค่ะ");
+
+
+                } else {
+
+                }
+            }   // onClick
+        });
+    }
+
+    private void bindWidget() {
+        nameEditText = (EditText) findViewById(R.id.editText3);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passEditText = (EditText) findViewById(R.id.editText5);
+        imageView = (ImageView) findViewById(R.id.imageView4);
+        radioGroup = (RadioGroup) findViewById(R.id.ragAvata);
+        button = (Button) findViewById(R.id.button3);
+
+    }
+}
